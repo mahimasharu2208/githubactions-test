b62cd95721ebbe0177c83344815d59adfe150217
Mahima <mahima.sharu@hashicorp.com>
2025-10-13 09:42:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform { 
+  cloud { 
+    
+    organization = "auth-learning" 
+
+    workspaces { 
+      name = "auto-apply-github-actions" 
+    } 
+  } 
+}
+    resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing"
+       }
+     }
+ resource "null_resource" "example2" {
+       triggers = {
+         value = "A example resource that again does nothing"
+       }
+     }
